e9d03965e06f31c0b6f9b30bcbb15833aee72df6
Alexandre Richard <alexandre.richard.dev@gmail.com>
2025-09-12 20:07:44 +0200

    feat#20 add errors object to test specs

--- specs/otherTest.specs.ts
+++ specs/otherTest.specs.ts
@@ -7,19 +7,40 @@ const otherTest = z.object({
 	isActive: z.boolean(),
 });
 
+const defaultErrors = z.object({
+	code: z.number(),
+	message: z.string(),
+});
+
 export const otherTestSpecs = makeApi([
 	{
 		method: "get",
 		path: "/test-other/all",
 		response: z.array(otherTest),
+		status: 200,
 		alias: "getOtherTests",
 		description: "Get all other test datas",
+		errors: [
+			{
+				status: 500,
+				description: "Servor error",
+				schema: defaultErrors,
+			},
+		],
 	},
 	{
 		method: "get",
 		path: "/test-other/unique",
 		response: otherTest,
+		status: 200,
 		alias: "getOtherTest",
 		description: "Get one other test data",
+		errors: [
+			{
+				status: 500,
+				description: "Servor error",
+				schema: defaultErrors,
+			},
+		],
 	},
 ] as const);
--- specs/test.specs.ts
+++ specs/test.specs.ts
@@ -7,19 +7,40 @@ const test = z.object({
 	isActive: z.boolean(),
 });
 
+const defaultErrors = z.object({
+	code: z.number(),
+	message: z.string(),
+});
+
 export const testSpecs = makeApi([
 	{
 		method: "get",
 		path: "/test/all",
 		response: z.array(test),
+		status: 200,
 		alias: "getTests",
 		description: "Get all test datas",
+		errors: [
+			{
+				status: 500,
+				description: "Servor error",
+				schema: defaultErrors,
+			},
+		],
 	},
 	{
 		method: "get",
 		path: "/test/unique",
 		response: test,
+		status: 200,
 		alias: "getTest",
 		description: "Get one test data",
+		errors: [
+			{
+				status: 500,
+				description: "Servor error",
+				schema: defaultErrors,
+			},
+		],
 	},
 ] as const);
