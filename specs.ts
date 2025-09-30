c8c63b93939ba1d134d959222cbb9486c5e83da8
Alexandre-RICHARD <alexandre.richard.dev@gmail.com>
2025-09-30 20:58:29 +0200

    essai 1

--- specs.ts
+++
@@ -1,7 +0,0 @@
-import { makeApi } from "@zodios/core";
-
-import { testSpecs } from "./project/testing/specs/test.specs.ts";
-
-const apiSpecs = makeApi([...testSpecs] as const);
-
-export default apiSpecs;
